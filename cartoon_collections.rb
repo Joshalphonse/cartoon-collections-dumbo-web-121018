581341986a606e4edd8e4f4e939b048698905824
Joshua Alphonse <joshalphonse@gmail.com>
2018-11-25 20:59:23 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,15 +4,18 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.map { |c| c.capitalize << "!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? { |w| w.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(str) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  str.find { |s| cheese_types.include?(s)}
 end
